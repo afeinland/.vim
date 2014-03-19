3e2ee75bb026f608dea6a8f3cc741820edbb188b
Alex Feinland <afein001@ucr.edu>
2014-03-19 14:55:29 -0700

    undo accidental dropbox sync

--- Dropbox/rails_projects/first_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '6d0b719b9fc159e76071e3c5ce8e802a0aa9882047bc0c8eb40386f81ff468794a558c0c87cb4427688a25027c7e5009ec388649a10596241a0d59eab392ba19'
